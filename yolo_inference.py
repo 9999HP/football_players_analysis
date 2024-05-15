86ecd855620def143f013beacace64e2e0f4965d
Fabien F. <124900622+9999HP@users.noreply.github.com>
2024-05-15 23:32:39 +0200

    Update yolo_inference.py

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,12 +1,15 @@
 from ultralytics import YOLO
 
 # We use the largest model to have the best accuracy
-model = YOLO('yolov8x')
+# model = YOLO('yolov8x')
+
+# Let's test our trained model
+model = YOLO('models/best.pt')
 
 # Run the model
 results = model.predict('input_videos/08fd33_4.mp4', save=True)
 print(results[0])
 
-# Print the value of each bounding boxes
+# Print the value of each bounding boxes for the first frame
 for box in results[0].boxes:
     print(box)
